9ed34c785ec0512eef42321ca22d9d678a8d6fad
shibukezhoude <2501835233@qq.com>
2022-09-22 21:56:51 +0800

    变更

--- app/src/main/java/com/example/myapplication/Activity/PersonInfoActivity.java
+++ app/src/main/java/com/example/myapplication/Activity/PersonInfoActivity.java
@@ -1,16 +1,33 @@
 package com.example.myapplication.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
 
 public class PersonInfoActivity extends AppCompatActivity {
+    private ImageView personInfo_arrow_right;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_personinfo);
+
+        init();
+    }
+
+    private void init() {
+        Button login_exit = findViewById(R.id.login_exit);
+        login_exit.setOnClickListener(v -> startActivity(new Intent(PersonInfoActivity.this,
+                LoginActivity.class)));
+
+        ImageView personInfo_backward = findViewById(R.id.iv_backward);
+        personInfo_backward.setOnClickListener(v -> startActivity(new Intent(PersonInfoActivity.this,
+                MainActivity.class)));
+
     }
 }

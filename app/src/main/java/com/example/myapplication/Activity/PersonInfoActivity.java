db7ac7134fc234441361cc6e9e580ecc6f5505e9
shibukezhoude <2501835233@qq.com>
2022-09-23 13:32:33 +0800

    变更

--- app/src/main/java/com/example/myapplication/Activity/PersonInfoActivity.java
+++ app/src/main/java/com/example/myapplication/Activity/PersonInfoActivity.java
@@ -4,20 +4,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.myapplication.Data.LoginData;
 import com.example.myapplication.R;
 
 public class PersonInfoActivity extends AppCompatActivity {
-    private ImageView personInfo_arrow_right;
-
+    private TextView tv_id;
+    private TextView tv_username;
+    private TextView tv_realName;
+    private TextView tv_idNumber;
+    private TextView tv_gender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personinfo);
 
         init();
+        initData();
     }
 
     private void init() {
@@ -29,5 +35,19 @@ public class PersonInfoActivity extends AppCompatActivity {
         personInfo_backward.setOnClickListener(v -> startActivity(new Intent(PersonInfoActivity.this,
                 MainActivity.class)));
 
+        tv_id = findViewById(R.id.tv_id_info);
+        tv_username = findViewById(R.id.tv_username_info);
+        tv_realName = findViewById(R.id.tv_realName_info);
+        tv_idNumber = findViewById(R.id.tv_idNumber_info);
+        tv_gender = findViewById(R.id.tv_gender_info);
+    }
+
+    private void initData(){
+        final boolean is_man = true;
+        tv_id.setText(String.valueOf(LoginData.loginUser.getId()));
+        tv_username.setText(LoginData.loginUser.getUsername());
+        tv_realName.setText(LoginData.loginUser.getRealName());
+        tv_idNumber.setText(String.valueOf(LoginData.loginUser.getIdNumber()));
+        //if(LoginData.loginUser.get)
     }
 }

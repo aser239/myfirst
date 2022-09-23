ab39127f1b6af3246d08ec6a5e3d3303cd1adbfa
shibukezhoude <2501835233@qq.com>
2022-09-23 11:37:23 +0800

    变更

--- app/src/main/java/com/example/myapplication/Interface/Api.java
+++ app/src/main/java/com/example/myapplication/Interface/Api.java
@@ -29,11 +29,12 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class Api {
-    public static  Gson gson = new Gson();
-    public static  String appId = "d44f6a157edc4815b907124907b98e63";
-    public static  String appSecret = "24416e30b926e08a54c7f93fded9670b769f3";
+    public static Gson gson = new Gson();
+    public static String appId = "d44f6a157edc4815b907124907b98e63";
+    public static String appSecret = "24416e30b926e08a54c7f93fded9670b769f3";
     public static int Code;
-    public static void enroll(String username,int roleId,String password){
+
+    public static void enroll(String username, int roleId, String password) {
         new Thread(() -> {
 
             // url路径
@@ -69,13 +70,13 @@ public class Api {
                 OkHttpClient client = new OkHttpClient();
                 //发起请求，传入callback进行回调
                 client.newCall(request).enqueue(ResponseBody.callback);
-            }catch (NetworkOnMainThreadException ex){
+            } catch (NetworkOnMainThreadException ex) {
                 ex.printStackTrace();
             }
         }).start();
     }
 
-    public static void login(String username,String password){
+    public static void login(String username, String password) {
         new Thread(() -> {
 
             // url路径
@@ -91,8 +92,8 @@ public class Api {
             // 请求体
             // PS.用户也可以选择自定义一个实体类，然后使用类似fastjson的工具获取json串
             FormBody.Builder params = new FormBody.Builder();
-            params.add("username",username); //添加url参数
-            params.add("password",password); //添加url参数
+            params.add("username", username); //添加url参数
+            params.add("password", password); //添加url参数
             // 将Map转换为字符串类型加入请求体中
 
             //请求组合创建
@@ -113,27 +114,28 @@ public class Api {
 
                     @Override
                     public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                        Type jsonType = new TypeToken<ResponseBody<Person>>(){}.getType();
+                        Type jsonType = new TypeToken<ResponseBody<Person>>() {
+                        }.getType();
                         // 获取响应体的json串
                         String body = response.body().string();
                         Log.d("info", body);
                         // 解析json串到自己封装的状态
-                        ResponseBody<Person> dataResponseBody = gson.fromJson(body,jsonType);
-                        LoginData.loginUser= dataResponseBody.getData();
+                        ResponseBody<Person> dataResponseBody = gson.fromJson(body, jsonType);
+                        LoginData.loginUser = dataResponseBody.getData();
                         Log.d("info", dataResponseBody.toString());
                         Log.d("Person:", LoginData.loginUser.getId());
                         Log.d("Person:", LoginData.loginUser.getUsername());
                     }
                 });
-            }catch (NetworkOnMainThreadException ex){
+            } catch (NetworkOnMainThreadException ex) {
                 ex.printStackTrace();
             }
         }).start();
     }
 
-    public static void AddCourse(String collegeName,String courseName,
-                                 String coursePhoto,String introduce,int endTime,
-                                 String realName,int startTime){
+    public static void AddCourse(String collegeName, String courseName,
+                                 String coursePhoto, String introduce, int endTime,
+                                 String realName, int startTime) {
         new Thread(() -> {
 
             // url路径
@@ -181,18 +183,90 @@ public class Api {
 
                     @Override
                     public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                        Type jsonType = new TypeToken<ResponseBody<Course>>(){}.getType();
+                        Type jsonType = new TypeToken<ResponseBody<Course>>() {
+                        }.getType();
                         // 获取响应体的json串
                         String body = response.body().string();
                         Log.d("info", body);
                         // 解析json串到自己封装的状态
-                        ResponseBody<Course> dataResponseBody = gson.fromJson(body,jsonType);
+                        ResponseBody<Course> dataResponseBody = gson.fromJson(body, jsonType);
                         Code = dataResponseBody.getCode();
                         CourseData.AddCourse = dataResponseBody.getData();
                         Log.d("info", dataResponseBody.toString());
                     }
                 });
-            }catch (NetworkOnMainThreadException ex){
+            } catch (NetworkOnMainThreadException ex) {
+                ex.printStackTrace();
+            }
+        }).start();
+    }
+
+    public static void AlterUserInfo(String collegeName, String realName,
+                                     boolean gender, String phone, String avatar,
+                                     int id, int idNumber, String userName,
+                                     String email, int inSchoolTime) {
+        new Thread(() -> {
+            // url路径
+            String url = "http://47.107.52.7:88/member/sign/course/teacher";
+
+            // 请求头
+            Headers headers = new Headers.Builder()
+                    .add("Accept", "application/json, text/plain, */*")
+                    .add("appId", appId)
+                    .add("appSecret", appSecret)
+                    .add("Content-Type", "application/json")
+                    .build();
+
+            // 请求体
+            // PS.用户也可以选择自定义一个实体类，然后使用类似fastjson的工具获取json串
+            Map<String, Object> bodyMap = new HashMap<>();
+            bodyMap.put("collegeName", collegeName);
+            bodyMap.put("realName", realName);
+            bodyMap.put("gender", gender);
+            bodyMap.put("phone", phone);
+            bodyMap.put("avatar", avatar);
+            bodyMap.put("id", id);
+            bodyMap.put("idNumber", idNumber);
+            bodyMap.put("userName", userName);
+            bodyMap.put("email", email);
+            bodyMap.put("inSchoolTime", inSchoolTime);
+            // 将Map转换为字符串类型加入请求体中
+            String body = gson.toJson(bodyMap);
+
+            MediaType MEDIA_TYPE_JSON = MediaType.parse("application/json; charset=utf-8");
+
+            //请求组合创建
+            Request request = new Request.Builder()
+                    .url(url)
+                    // 将请求头加至请求中
+                    .headers(headers)
+                    .post(RequestBody.create(MEDIA_TYPE_JSON, body))
+                    .build();
+            try {
+                OkHttpClient client = new OkHttpClient();
+                //发起请求，传入callback进行回调
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                        Type jsonType = new TypeToken<ResponseBody<Course>>() {
+                        }.getType();
+                        // 获取响应体的json串
+                        assert response.body() != null;
+                        String body = response.body().string();
+                        Log.d("info", body);
+                        // 解析json串到自己封装的状态
+                        ResponseBody<Course> dataResponseBody = gson.fromJson(body, jsonType);
+                        Code = dataResponseBody.getCode();
+                        CourseData.AddCourse = dataResponseBody.getData();
+                        Log.d("info", dataResponseBody.toString());
+                    }
+                });
+            } catch (NetworkOnMainThreadException ex) {
                 ex.printStackTrace();
             }
         }).start();

288673296943648392841aeb7ead6292b5905bff
TengYuanWenTai <2941912132@qq.com>
2022-10-07 16:42:25 +0800

    导航栏

--- app/src/main/java/com/example/myapplication/Activity/EnterSignInActivity.java
+++ app/src/main/java/com/example/myapplication/Activity/EnterSignInActivity.java
@@ -11,6 +11,6 @@ public class EnterSignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_enter_sign_in);
+
     }
 }
